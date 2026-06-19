4c6a504cc8ede60e27b5df1a400a5a3319fa3b9a
Alirzeanoroozi <noroozi.alirezaa@gmail.com>
2026-06-19 10:25:22 +0300

    start rewriting boltz

---
+++ test.py
@@ -0,0 +1,15 @@
+# import torch
+
+# device_capability = torch.cuda.get_device_capability()
+
+# print(device_capability)
+
+import omegaconf
+import hydra
+from boltzgen.task.task import Task
+
+config = omegaconf.OmegaConf.load("src/boltzgen/resources/config/design.yaml")
+task = hydra.utils.instantiate(config)
+if not isinstance(task, Task):
+    raise TypeError("Config must be an instance of Task.")
+task.run(config)
